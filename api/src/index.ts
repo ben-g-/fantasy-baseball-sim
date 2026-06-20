8e6cb48fe03247e10d9f6f738f4bb1336b7661cc
ben-g- <gottesman.ben@gmail.com>
2026-06-20 12:44:57 +0200

    make the dev endpoints be opt-in rather than opt-out
    gate dev endpoints on the existence of an environment variable rather than the non-existence of one, so the default is they aren't available -- safer

--- api/src/index.ts
+++ api/src/index.ts
@@ -11,7 +11,7 @@ app.use(cors({ origin: process.env.CORS_ORIGIN ?? '*' }));
 app.use(express.json());
 
 app.use('/api/v1', meRouter);
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.DEV_ENDPOINTS_ENABLED === 'true') {
   app.use('/api/v1', devRouter);
 }
 
