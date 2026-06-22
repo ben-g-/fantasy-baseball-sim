d2dbad097cce13d507146de6b0114262586f1b28
ben-g- <gottesman.ben@gmail.com>
2026-06-22 09:33:01 +0200

    use Inter font

--- web/src/App.vue
+++ web/src/App.vue
@@ -7,6 +7,8 @@ body {
   background-color: var(--p-surface-50);
   min-height: 100vh;
   margin: 0;
+  font-family: 'Inter', system-ui, 'Segoe UI', sans-serif;
+  -webkit-font-smoothing: antialiased;
 }
 
 .surface-card {
